96d4dee250f0543c103fb99cf309341efdd3c075
Breno Pereira <brenomartinsxd@gmail.com>
2019-11-19 10:46:14 -0400

    TODO for dev in next time

---
+++ src/screens/accounts/gamification/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class GamificationScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Show progress from user with XP and LVL </Text>
+      </View>
+    );
+  }
+}
---
+++ src/screens/quizzes/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class QuizzesScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> List quizzes from platform </Text>
+      </View>
+    );
+  }
+}
